4b14f562d5a7faeb3ed05f07640daeb24311583d
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-10-28 14:03:45 +0100

    Initial commit

---
+++ music_collector.py
@@ -0,0 +1,25 @@
+import csv
+
+
+def main():
+    data = import_files()
+    print(data)
+    list_of_lists = data
+    print()
+    print_records(list_of_lists)
+
+
+def import_files(filename='text_albums_data.txt'):
+    results = []
+    with open(filename, newline='') as inputfile:
+        for row in csv.reader(inputfile):
+            results.append(row)
+    return results
+
+
+def print_records(list_of_lists):
+    for elem in list_of_lists:
+        print(elem)
+
+
+main()
