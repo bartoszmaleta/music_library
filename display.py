8c0ac09c714e1e7205f763b9ad5098249ab41c8f
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-10-31 00:51:04 +0100

    Add many cosmetics changes (visual program)

--- display.py
+++ display.py
@@ -1,13 +1,24 @@
+import time
 user_choice = ''
 
 
 def print_records(list_of_lists):
+    text_for_printing_the_whole_list_of_lists = 'THE LIST OF ALBUMS \n'
+    text_for_printing_the_whole_list_of_lists_alignment = text_for_printing_the_whole_list_of_lists.center(50)
+    print(text_for_printing_the_whole_list_of_lists_alignment)
     for elem in list_of_lists:
         print(elem)
     return print_records
 
 
 def display_menu():
+    print('=========================================================')
+    print('=========================================================')
+    menu_text = '\033[1;35;49m MENU!'
+    menu_text_alignment = menu_text.center(63)
+    print(menu_text_alignment)
+    print('\033[0;37;49m ')
+    print('=========================================================')
     print("1. Show list")
     print("2. Find all albums by genre.")
     print("3. Find all albums from given time range.")
@@ -17,6 +28,46 @@ def display_menu():
     print("7. Print report")
     print("8. Add new album")
     print("9. Edit album")
-    print("10. Find oldest or youngest album (o/y)")
+    print("10. Find oldest or youngest album")
     print("Q. Quit")
 
+
+def welcoming_screen():
+    print()
+    print('                        ##########################################')  # spaces = 20
+    user_name_question = ('                        ########### What is your name? ###########')
+    print(user_name_question)
+    print('                        ##########################################')
+    print()
+    print('_______________')
+    print('|')
+    user_name_answer = input('| ')
+    print('|______________')
+    clear_terminal()
+    print()
+    hello_text = ('Hello {}'.format(user_name_answer))
+    hello_text_alignment = hello_text.ljust(60)
+    print(hello_text_alignment)
+    print()
+    print()
+    welcome_text = '\033[1;33;49m Welcome in the MUSIC COLLECTOR!'
+    welcome_text_alignment = welcome_text.center(100)
+    copyrights_text = '\033[1;32;49m Michał Z., Bartosz M.'
+    copyrights_alignment = copyrights_text.center(100)
+    print(welcome_text_alignment)
+    print(copyrights_alignment)
+    print('\033[0;37;49m \n')
+    time.sleep(1)         # change to 5
+    clear_terminal()
+
+
+def clear_terminal():
+    print('\033[H\033[J')
+
+
+def your_choice():
+    print("Enter your choice: ")
+
+
+def one_line_space():
+    print()
--- music_collector.py
+++ music_collector.py
@@ -3,42 +3,62 @@ import csv
 from display import *
 import time
 
+# INFO: MICHAŁ!!!!!! Don't change path!!! Just comment my path, and uncomment yours, which is below
+# CHANGE time.sleep in display to 5
+
 # TODO:
-# - Last point of report = how many albums are given the genres
-# - As a user I want to view all similar music genres albums when searching for particular album (Like in spotify, where user receives context suggestions regarding similar bands or tracks)
-# - As a user I want to add new album ===> DONE partially, does not update print_tabel during running program
-# - As a user I want to edit already existing albums
-# - As a user I want to save all new albums to external file
+# 1) Last point of report ===> DONE partially ==>  how many albums are given the genres
+# 2) As a user I want to view all similar music genres albums when searching 
+#    for particular album (Like in spotify, where user receives context suggestions 
+#    regarding similar bands or tracks)
+# 3) As a user I want to add new album ===> DONE partially ==> does not update print_tabel 
+#                                                              during running program
+# 4) As a user I want to edit already existing albums
+# 5) As a user I want to save all new albums to external file
+#
+# TODO: Features not in exercises:
+# 6) Pretty tables!!!!! 
 
 
 def main():
+    welcoming_screen()
     menu()
-   
+
 
 def menu():
     is_running = True
     while is_running:
         import_files()
         display_menu()
-        user_choice = input("Your choise is: ")
-        if (user_choice == "1"):
+        your_choice()
+        user_choice = input()
+        if (user_choice == "1"): 
+            clear_terminal()
+            # 3 TODO: here something add to update list_of_lists 
             print_records(list_of_lists)
         elif (user_choice == "2"):
+            clear_terminal()
             print_albums_by_genre()
         elif (user_choice == "3"):
+            clear_terminal()
             print_albums_by_given_time_range()
         elif (user_choice == "4"):
+            clear_terminal()
             printing_shortest_or_longest_album()
         elif (user_choice == "5"):
+            clear_terminal()
             print_albums_by_given_artist()
         elif (user_choice == "6"):
+            clear_terminal()
             print_albums_by_given_album_name()
         elif (user_choice == "7"):  # TODO raport!!!! 
-            # printning_raport()
-            print_raport()
-        elif (user_choice == "8"):
+            clear_terminal()
+            print_report()
+        elif (user_choice == "8"):  # TODO: ADD ==> updating list_of_lists after adding
+            clear_terminal()
             add_new_album()
         elif (user_choice == "10"):
+            clear_terminal()
             print_youngest_or_oldest_album()
         elif (user_choice == "Q"):
             is_running = False
@@ -56,16 +76,15 @@ def import_files(filename="/home/acer/Documents/music_library/music_library/text
 # filename = "text_albums_data.txt"
 data = import_files()
 import_files()
-print(data)
+# print(data)
 list_of_lists = data
 
 
-# ----------------------------------------------
+# ---------------------------------------------- GENRE 
 
 def return_albums_by_genre(list_of_lists):
     genre = input('By what genre you want see albums? ')
     genre = genre.lower()
-    
     list_of_albums_sorted_by_genre = []
     
     for elem in list_of_lists:
@@ -80,23 +99,28 @@ def return_albums_by_genre(list_of_lists):
 
 def print_albums_by_genre():
     albums_by_genre = return_albums_by_genre(list_of_lists)
+    one_line_space()
+    print('Albums with your genre are: ')
+    print('--------------------------------------')    
+    for elem in albums_by_genre:
+        print(elem)
+    print('--------------------------------------')    
     print()
-    print('Albums with your genre are: \n {}'.format(albums_by_genre))
     print()
     # print_records(list_of_albums_sorted_by_genre)
 
 
-# ----------------------------------------------
+# ---------------------------------------------- TIME RANGE
 
 def return_albums_by_given_time_range(list_of_lists):
     minimal_time_in_minutes = (input('Minimum time of album: (in minutes) '))
     maximum_time_in_minutes = (input('Maximum time of album: (in minutes) '))
-    print(minimal_time_in_minutes)
-    print(maximum_time_in_minutes)
+    # print(minimal_time_in_minutes)
+    # print(maximum_time_in_minutes)
     minimal_time_in_seconds = int(minimal_time_in_minutes) * 60
-    print(minimal_time_in_seconds)
+    # print(minimal_time_in_seconds)
     maximum_time_in_seconds = int(maximum_time_in_minutes) * 60
-    print(maximum_time_in_seconds)  
+    # print(maximum_time_in_seconds)  
 
     list_of_albums_sorted_by_given_time_range = []
 
@@ -105,7 +129,7 @@ def return_albums_by_given_time_range(list_of_lists):
         (m, s) = length_of_album.split(':')  
         length_of_album_in_seconds = int(m) * 60 + int(s)
         if length_of_album_in_seconds in range(minimal_time_in_seconds, maximum_time_in_seconds):
-            print(elem)
+            # print(elem)
             list_of_albums_sorted_by_given_time_range.append(elem)
 
     # print_records(list_of_albums_sorted_by_given_time_range)
@@ -115,12 +139,19 @@ def return_albums_by_given_time_range(list_of_lists):
 
 def print_albums_by_given_time_range():
     albums_by_given_time_range = return_albums_by_given_time_range(list_of_lists)
-    print()
-    print('Albums with your given range are: \n {}'.format(albums_by_given_time_range))
-    print()
+    one_line_space()
+    info_about_what_is_displaying = 'Albums with your given range are: '
+    print(info_about_what_is_displaying)
+    print('--------------------------------------')    
+    for elem in albums_by_given_time_range:
+        print(elem)
+    print('--------------------------------------')    
+    # print()
+    # print(' \n {}'.format(albums_by_given_time_range))
+    # print()
 
 
-# ----------------------------------------------
+# ---------------------------------------------- ALBUMS BY ARTIST
 
 def return_albums_by_artist(list_of_lists):
     artist = input('By what artist you want see albums? ')
@@ -138,15 +169,19 @@ def return_albums_by_artist(list_of_lists):
 
 def print_albums_by_given_artist():
     albums_by_given_artist = return_albums_by_artist(list_of_lists)
-    print()
-    print('Albums with your given artist are: \n {}'.format(albums_by_given_artist))
-    print()
+    one_line_space()
+    info_about_what_is_displaying = 'Albums with your given artist name are: '
+    print(info_about_what_is_displaying)
+    print('--------------------------------------')    
+    for elem in albums_by_given_artist:
+        print(elem)
+    print('--------------------------------------')    
 
 
-# ----------------------------------------------
+# ---------------------------------------------- BY GIVEN NAME
 
 def return_albums_by_given_album_name(list_of_lists):
-    album_name = input('By what name you want see albums? ')
+    album_name = input('By what album name you want see albums? ')
     list_of_albums_sorted_by_album_name = []
     # album_name = album_name.lower()
     for elem in list_of_lists:
@@ -161,12 +196,16 @@ def return_albums_by_given_album_name(list_of_lists):
 
 def print_albums_by_given_album_name():
     albums_by_given_album_name = return_albums_by_given_album_name(list_of_lists)
-    print()
-    print('Albums with your given album name are: \n {}'.format(albums_by_given_album_name))
-    print()
+    one_line_space()
+    info_about_what_is_displaying = 'Albums with your given album name are: '
+    print(info_about_what_is_displaying)
+    print('--------------------------------------')    
+    for elem in albums_by_given_album_name:
+        print(elem)
+    print('--------------------------------------')  
 
 
-# -------------------------------
+# ---------------------------------------------- OLDEST
 
 def return_list_of_years(list_of_lists):
     list_of_years = []
@@ -194,7 +233,7 @@ def return_list_of_oldest_album():
     return list_of_oldest_album
 
 
-# ----------------------------------------------
+# ---------------------------------------------- YOUNGEST 
 
 def return_list_of_youngest_album():
     max_years = []
@@ -218,17 +257,26 @@ def return_list_of_youngest_album():
 def print_youngest_or_oldest_album():
     # list_of_lists = data
     print_youngest_album = return_list_of_youngest_album()
-
     print_oldest_album = return_list_of_oldest_album()
 
     max_or_min = input('Oldest or Youngest? (o/y) ')
     if max_or_min == 'o':
-        print(print_oldest_album)
+        one_line_space()
+        info_about_what_is_displaying = 'Oldest albums are: '
+        print(info_about_what_is_displaying)
+        for elem in print_oldest_album:
+            print(elem)
+        # print(print_oldest_album)
     elif max_or_min == 'y':
-        print(print_youngest_album)
+        one_line_space()
+        info_about_what_is_displaying = 'Newest albums are: '
+        print(info_about_what_is_displaying)
+        for elem in print_youngest_album:
+            print(elem)
+        # print(print_youngest_album)
 
 
-# ----------------------------------------------
+# ---------------------------------------------- LONGEST / SHORTEST
 
 def return_list_of_length_of_albums(list_of_lists):
     list_of_length_of_albums = []
@@ -268,32 +316,56 @@ def printing_shortest_or_longest_album():
 
     shortest_or_longest = input('Shortest or Longest? (s/l) ')
     if shortest_or_longest == 's':
+        one_line_space()
+        info_about_what_is_displaying = 'Albums with shortest length are: '
+        print(info_about_what_is_displaying)
+
+        # for elem in print_list_of_shortest_albums:
+            # print(elem)
         print(print_list_of_shortest_albums)
     elif shortest_or_longest == 'l':
+        one_line_space()
+        info_about_what_is_displaying = 'Albums with longest length are: '
+        print(info_about_what_is_displaying)
+
+        # for elem in print_list_of_longest_albums:
+            # print(elem)
         print(print_list_of_longest_albums)
 
 
-# ----------------------------------------------
+# ---------------------------------------------- REPORT
 
-def print_raport():
+def print_report():
     print_youngest_album = return_list_of_youngest_album()
     print_oldest_album = return_list_of_oldest_album()
     print_list_of_shortest_albums = return_list_of_shortest_albums()
     print_list_of_longest_albums = return_list_of_longest_albums()
 
-    print('ya ---> ', print_youngest_album, '\n', 'old ----> ', print_oldest_album)
-    print('shortest ---> ', print_list_of_shortest_albums)
-    print('longest ---> ', print_list_of_longest_albums)
+    report_text = '\033[1;34;49m REPORT'
+    report_text = report_text.center(60)
+    print(report_text)
+    print('\033[0;37;49m ')
+    print('Newest album: \n')
+    for elem in print_youngest_album:
+        print('', elem)
+    print('---------------------')
+    print('Oldest album: \n') 
+    for elem in print_oldest_album:
+        print('', elem)
+    print('---------------------')
+    print('Shortest album: \n\n', print_list_of_shortest_albums)
+    print('---------------------')
+    print('Longest album: \n\n', print_list_of_longest_albums)
     print('---------------------')
     list_of_length_of_all_albums = return_list_of_length_of_albums(list_of_lists)
     # print(list_of_length_of_all_albums)
     sum_of_list_of_length_of_all_albums = sum(list_of_length_of_all_albums)
     # print(sum_of_list_of_length_of_all_albums)
     length_of_all_albums = (time.strftime("%H:%M:%S", time.gmtime(sum_of_list_of_length_of_all_albums)))
-    print('length of all albums: ', length_of_all_albums)
+    print('Length of all albums: \n\n', length_of_all_albums)
 
 
-# ----------------------------------------------
+# ---------------------------------------------- ADDING NEW ALBUM
 
 def add_new_album():
     new_album = []
